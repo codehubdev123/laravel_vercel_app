0715e135174e5f7987ef8dfce5764a32f8bf7c5e
Admin <admin@admin.com>
2025-01-28 21:33:02 +0200

    Add test api links

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testget', function () {
+    $data = [
+        'id' => 1,
+        'name' => 'ahmed',
+        'email' => 'test@mail.com',
+    ];
+    return response()->json([
+        'status' => true,
+        'message' => 'Hello from get',
+        'data' => $data,
+    ]);
+});
+Route::post('/testpost', function () {
+    return response()->json(['status' => true, 'message' => 'Data has been created']);
+});

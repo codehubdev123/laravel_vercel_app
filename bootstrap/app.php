699accd98d526915eb62b8cf8c4f28c5d7561267
Admin <admin@admin.com>
2025-01-29 11:02:00 +0200

    AGAIN EDIT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,18 +8,14 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-        using: function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-        }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            'api/*',
-        ]);
+        // $middleware->validateCsrfTokens(except: [
+        // 'api/*',
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
